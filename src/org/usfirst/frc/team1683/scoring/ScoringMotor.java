389111123be46293f01c96978a894eceea59ba9e
Nidhi Pai <nidhi.pai@gmail.com>
2018-01-11 16:51:49 -0500

    basic scoring

---
+++ src/org/usfirst/frc/team1683/scoring/ScoringMotor.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team1683.scoring;
+
+/**
+ * 
+ * Scoring motors interface
+ * 
+ * @author Yi Liu
+ *
+ */
+
+public interface ScoringMotor {
+		
+	public void turnOn();
+
+	public void stop();
+}
