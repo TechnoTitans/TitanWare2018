fadc0871b95b5062782cf16fb038d901f040c429
Nidhi Pai <nidhi.pai@gmail.com>
2018-01-13 14:03:35 -0500

    elevator

---
+++ src/org/usfirst/frc/team1683/scoring/Elevator.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team1683.scoring;
+
+import org.usfirst.frc.team1683.constants.HWR;
+import org.usfirst.frc.team1683.driverStation.DriverSetup;
+import org.usfirst.frc.team1683.motor.TalonSRX;
+import org.usfirst.frc.team1683.sensors.Encoder;
+
+public class Elevator {
+	
+	private TalonSRX elevatorMotor;	
+	private double liftSpeed = .7;
+	
+	public Elevator(int channel) {
+		this.elevatorMotor = new TalonSRX(channel, true);
+	}
+	
+	public void run() {
+		if(DriverSetup.auxStick.getRawButton(8)) //fix
+			elevatorMotor.set(liftSpeed);
+		else
+			elevatorMotor.set(0);
+		
+		if(DriverSetup.auxStick.getRawButton(2))
+			elevatorMotor.set(-liftSpeed);
+		else
+			elevatorMotor.set(0);
+	}
+	
+	public TalonSRX getMotor() {
+		return elevatorMotor;
+	}
+
+}
